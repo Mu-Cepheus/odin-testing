3a260251241ebbab02fd3f83eec8afa590f79732
Mu-Cepheus <>
2025-01-31 23:46:27 +0800

    feat: Write caesarCipher function
    test: Add test suites for caesarCipher
    
    refactor: specify es6 module type in package json
    
    Use "type":"module" to specify es6 modules in package.json
    Modify npm watch script to point to update tests folder
    Change babel config file type to coerce commonJS module exports

--- src/scripts/caesarCipher.js
+++ src/scripts/caesarCipher.js
@@ -1,1 +1,12 @@
-export default function caesarCipher() {}
+export default function caesarCipher(argString, argShift) {
+  let encrypted = [];
+  for (let i = 0; i < argString.length; i++) {
+    const char = argString.charCodeAt(i);
+    if (char >= 65 && char <= 90)
+      encrypted.push(String.fromCharCode(((char - 65 + argShift) % 26) + 65));
+    else if (char >= 97 && char <= 122)
+      encrypted.push(String.fromCharCode(((char - 97 + argShift) % 26) + 97));
+    else encrypted.push(argString[i]);
+  }
+  return encrypted.join("");
+}
--- src/tests/caesarCipher.test.js
+++ src/tests/caesarCipher.test.js
@@ -1,5 +1,13 @@
 import caesarCipher from "./../scripts/caesarCipher.js";
 
-test("Encrypts text", () => {
+test("Preserves cases", () => {
+  expect(caesarCipher("HeLLo", 3)).toBe("KhOOr");
+});
+
+test("Alphabet wrapping", () => {
+  expect(caesarCipher("xyz", 3)).toBe("abc");
+});
+
+test("Preserves punctuation", () => {
   expect(caesarCipher("Hello, World!", 3)).toBe("Khoor, Zruog!");
 });
